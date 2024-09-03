d641384dfee5f2d36aa2ff87b5c9c3578be41243
Mina <minamagdysh2017@gmail.com>
2024-09-03 12:45:57 +0300

    prepare for deployment

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,37 +1,37 @@
 import React, { useEffect, useRef, useState, useCallback } from 'react'
 import repeat from "../assets/iconrepeat.png"
-import {AiOutlineUser} from 'react-icons/ai'
-import {BsHeartFill, BsLayoutSidebar} from 'react-icons/bs'
-import {BsHeart} from 'react-icons/bs'
+import { AiOutlineUser } from 'react-icons/ai'
+import { BsHeartFill, BsLayoutSidebar } from 'react-icons/bs'
+import { BsHeart } from 'react-icons/bs'
 import { useDispatch, useSelector } from 'react-redux'
 import { getAllCars, likeOrUnlikeAcar } from '../store/reducer/carSlice'
 
-const Card = ({theCar}) => {
-  let {model,type,numberOfClients,dependency,rentPrice,imageSrc,id,isLiked}=theCar
-  let {cars}=useSelector(state=>state.carList)
-  let dispatch=useDispatch()
-  let [heartColor,setHeartColor]=useState(`white`)
+const Card = ({ theCar }) => {
+  let { model, type, numberOfClients, dependency, rentPrice, imageSrc, id, isLiked } = theCar
+  let { cars } = useSelector(state => state.carList)
+  let dispatch = useDispatch()
+  let [heartColor, setHeartColor] = useState(`white`)
 
-  let toggleSpan=()=>{
-    dispatch(likeOrUnlikeAcar([id,{...theCar,isLiked:!isLiked}]))
-    }
+  let toggleSpan = () => {
+    dispatch(likeOrUnlikeAcar([id, { ...theCar, isLiked: !isLiked }]))
+  }
 
-  let hoverOnWhiteHeart=(e)=>{
-    if(!isLiked){
+  let hoverOnWhiteHeart = (e) => {
+    if (!isLiked) {
       setHeartColor(`red`)
     }
   }
-  
 
-  let mouseLeaveHandler=(e)=>{
-    if(!isLiked){
+
+  let mouseLeaveHandler = (e) => {
+    if (!isLiked) {
       setHeartColor(`white`)
     }
   }
 
-  useEffect(()=>{
-    dispatch(likeOrUnlikeAcar(id,theCar))
-  },[])
+  // useEffect(() => {
+  //   dispatch(likeOrUnlikeAcar(id, theCar))
+  // }, [])
 
   return (
     <div className="container col-12 col-sm-6 col-lg-4 p-2">
@@ -39,11 +39,11 @@ const Card = ({theCar}) => {
         <div className="d-flex justify-content-between w-100">
           <h5>{model}</h5>
           <span
-            onClick={()=>toggleSpan()}
-            onMouseEnter={(e)=>hoverOnWhiteHeart(e)}
+            onClick={() => toggleSpan()}
+            onMouseEnter={(e) => hoverOnWhiteHeart(e)}
             onMouseLeave={mouseLeaveHandler}
           >
-            {isLiked || heartColor===`red`?<BsHeartFill className='red-heart'/>:<BsHeart/>}</span>
+            {isLiked || heartColor === `red` ? <BsHeartFill className='red-heart' /> : <BsHeart />}</span>
         </div>
 
         <div className="d-flex w-100">
@@ -57,12 +57,12 @@ const Card = ({theCar}) => {
         <div className="d-flex justify-content-between w-100 mt-2">
           <div className='d-flex justify-content-start gap-3 col-9'>
             <div>
-              <span><AiOutlineUser className='user-icon'/></span>
+              <span><AiOutlineUser className='user-icon' /></span>
               <span className='ms-1 mb-0 text-end'>{numberOfClients}</span>
             </div>
             <div className='d-flex gap-1'>
-            <span><img src={repeat} alt="" /></span>
-            <span>{dependency}</span>
+              <span><img src={repeat} alt="" /></span>
+              <span>{dependency}</span>
             </div>
           </div>
 
